241bf1b0a68c16c12ecb5da478015a2c2bac8da7
Amrindra <amrindraouk@yahoo.com>
2021-03-09 00:30:23 -0500

    update the codes to use the passport.js to add cookies and sessions

--- app.js
+++ app.js
@@ -1,75 +1,107 @@
-
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const app =  express();
+const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(session({
+  secret: "My little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-// encryption section
-// encrypting password field in the database
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
+// this is we use to "hash and salt the password" save the users into the database
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    // if users successfully register, they will be able to see the secret page
+// this section is used when the users already login and never logout, next time they goto this website they dont have to login again unless they logout
+app.get("/secrets", function(req, res){
+  // if the user the is authenticated they will bring to the secret page, else they will be redirected back to "login"
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/")
+})
+
+app.post("/register", function(req, res) {
+  // "register()" comes with the passport package
+  // "{username: req.body.username}, req.body.password" are used to capture username and password when user type in
+  User.register({username: req.body.username}, req.body.password, function(err, user){
     if (err){
       console.log(err);
+      res.redirect("/register");
     }else{
-      res.render("secrets");
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
 });
 
-app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password =  req.body.password;
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.passowrd
+  });
 
-  User.findOne({email: username}, function(err, foundUser){
-    if (err){
+  req.logIn(user, function(err){
+    if(err){
       console.log(err);
     }else{
-      if (foundUser){
-        // if the found password match the password user type in then they allow to log in
-        if (foundUser.password === password){
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
+
 });
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
